27917bf97423b0d234c1901308a8eeabdc711bce
Stefan Dedic <itsthededics@gmail.com>
2026-05-02 19:37:20 -0400

    Updated and worked on notification service features

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
-# Notification Model for MongoDB DB if we implement
-
 class Notification:
-    def __init__(self, message) -> None:
+    def __init__(self, notification_id: int, message: str):
+        self.id = notification_id
         self.message = message
 
     def to_dict(self) -> dict:
         return {
+            "id": self.id,
             "message": self.message
         }
